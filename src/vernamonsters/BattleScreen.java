30c0933d1f9d7976da5967c9e3934dc19e383179
Sweord <Sweord@Sweord-PC>
2012-05-15 18:18:29 -0700

    Set up images in a hashMap (ResourceMap.java) instead of having functions for loading and setting each one. Fixed a resize bug where position of the background panel was set according to the BattleFrame's position relative to the top of the screen, even though it's coordinates are relative to it's parent container.

--- src/vernamonsters/BattleScreen.java
+++ src/vernamonsters/BattleScreen.java
@@ -4,16 +4,12 @@
  */
 package vernamonsters;
 
-import java.awt.Graphics;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.Image;
-import java.awt.LayoutManager;
 import java.awt.Rectangle;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -29,13 +25,87 @@ import javax.swing.JPanel;
 public class BattleScreen extends JFrame {
 
     final static long serialVersionUID = 60l;
-    Image BackGround;
-    JPanel mainPanel;
-    File backGroundSource;
-    Rectangle TopLeft, TopRight, BottomLeft, BottomRight;
     ImagePanel backgroundPanel;
     HealthBar playerHealth, opponentHealth;
-    Image PlayerPortrait, OpponentPortrait;
+    ResourceMap BattleResources;
+
+    
+
+
+    private void Initialize() {
+        try {
+            //Set window parameters
+            this.setLayout(null);
+            this.setSize(640, 480);
+            this.setVisible(true);
+            this.setResizable(true);
+            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            this.setTitle("BATTLE!");
+
+            //Load up the images and initialize the battle resources
+            BattleResources = new ResourceMap();
+            BattleResources.addImage("/vernamonsters/landscape2.png","Background");
+            BattleResources.addImage("/vernamonsters/landscape2.png","Player Portrait");
+
+            //Add the background panel
+            backgroundPanel = new ImagePanel(BattleResources.getImage("Background"));
+            this.add(backgroundPanel);
+            resizeBackground();
+            backgroundPanel.setLayout(null);
+
+            //Add a healthbar to the background panel
+            playerHealth = new HealthBar(1.0f);
+            opponentHealth = new HealthBar(1.0f);
+
+            backgroundPanel.add(playerHealth);
+            backgroundPanel.add(opponentHealth);
+
+            playerHealth.setBounds(10, (this.getHeight() / 2), (int) (this.getWidth() * 0.4), 15);
+            opponentHealth.setBounds((int) (this.getBounds().getWidth() / 2), 10, (int) (this.getWidth() * 0.4), 15);
+
+
+
+            this.addComponentListener(BattleComponentListener);
+            System.out.println("Painting");
+            repaint();
+
+
+
+
+        } catch (Exception ex) {
+            Logger.getLogger(BattleScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void resizeBackground(){
+        backgroundPanel.setBounds(0,0,this.getWidth(),this.getHeight());
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="Listener Implementations">
+    ComponentListener BattleComponentListener = new ComponentListener() {
+
+        public void componentResized(ComponentEvent e) {
+            resizeBackground();
+            playerHealth.setBounds(10, (backgroundPanel.getHeight() / 2), (int) (backgroundPanel.getWidth() * 0.4), 15);
+            opponentHealth.setBounds((int) (backgroundPanel.getBounds().getWidth() / 2), 10, (int) (backgroundPanel.getWidth() * 0.4), 15);
+            System.out.println("Resized!");
+            repaint();
+        }
+
+        public void componentMoved(ComponentEvent e) {
+            repaint();
+        }
+
+        public void componentShown(ComponentEvent e) {
+            resizeBackground();
+            repaint();
+        }
+
+        public void componentHidden(ComponentEvent e) {
+            repaint();
+        }
+    };
+    // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Constructors">
     public BattleScreen(String title, GraphicsConfiguration gc) {
@@ -59,148 +129,4 @@ public class BattleScreen extends JFrame {
         Initialize();
     }// </editor-fold>
 
-    private void Initialize() {
-        try {
-            //Set window parameters
-            this.setLayout(null);
-            this.setSize(640, 480);
-            this.setVisible(true);
-            this.setResizable(true);
-            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            this.setTitle("BATTLE!");
-            
-            //Load up the background image
-            this.setBackGround("/vernamonsters/landscape2.png");
-            this.setPlayerPortrait("/vernamonsters/landscape2.png");
-
-            //Add the background panel
-            backgroundPanel = new ImagePanel(BackGround);
-            this.add(backgroundPanel);
-            backgroundPanel.setBounds(this.getBounds());
-            backgroundPanel.setLayout(null);
-
-            //Add a healthbar to the background panel
-            playerHealth = new HealthBar(1.0f);
-            opponentHealth = new HealthBar(1.0f);
-
-            backgroundPanel.add(playerHealth);
-            backgroundPanel.add(opponentHealth);
-            
-            playerHealth.setBounds(10,(this.getHeight()/2),(int)(this.getWidth()*0.4),15);
-            opponentHealth.setBounds((int)(this.getBounds().getWidth()/2),10,(int)(this.getWidth()*0.4),15);
-
-            
-
-            this.addComponentListener(BattleComponentListener);
-            System.out.println("Painting");
-            repaint();
-
-
-
-
-        } catch (Exception ex) {
-            Logger.getLogger(BattleScreen.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public void setBackGround(Image BackGround) {
-        this.BackGround = BackGround;
-    }
-
-
-
-    /**
-     *
-     * @param resource
-     */
-    public void setBackGround(String resource) {
-        try {
-            this.BackGround = ImageIO.read(this.getClass().getResource(resource));
-        } catch (IOException ex) {
-            Logger.getLogger(BattleScreen.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-
-    private void setPlayerPortrait(String resource) {
-        try {
-            this.PlayerPortrait = ImageIO.read(this.getClass().getResource(resource));
-        } catch (IOException ex) {
-            Logger.getLogger(BattleScreen.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-
-    @Override
-    public void paint(Graphics g) {
-        super.paintComponents(g);
-
-
-
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="Listener Implementations">
-    WindowListener BattleWindowListener = new WindowListener() {
-
-        public void windowOpened(WindowEvent e) {
-            System.out.println("BattleWindow Opened");
-            repaint();
-        }
-
-        public void windowClosing(WindowEvent e) {
-            System.out.println("BattleWindow Closing");
-            repaint();
-        }
-
-        public void windowClosed(WindowEvent e) {
-            System.out.println("BattleWindow Closed");
-            repaint();
-        }
-
-        public void windowIconified(WindowEvent e) {
-            System.out.println("BattleWindow Iconified");
-            repaint();
-        }
-
-        public void windowDeiconified(WindowEvent e) {
-            System.out.println("BattleWindow DeIconified");
-            repaint();
-        }
-
-        public void windowActivated(WindowEvent e) {
-            System.out.println("BattleWindow Activated");
-            repaint();
-        }
-
-        public void windowDeactivated(WindowEvent e) {
-            System.out.println("BattleWindow Deactivated");
-            repaint();
-        }
-    };
-    ComponentListener BattleComponentListener = new ComponentListener() {
-
-        public void componentResized(ComponentEvent e) {
-            backgroundPanel.setBounds(getBounds());
-
-            playerHealth.setBounds(10,(backgroundPanel.getHeight()/2),(int)(backgroundPanel.getWidth()*0.4),15);
-            opponentHealth.setBounds((int)(backgroundPanel.getBounds().getWidth()/2),10,(int)(backgroundPanel.getWidth()*0.4),15);
-            System.out.println("Resized!");
-            repaint();
-        }
-
-        public void componentMoved(ComponentEvent e) {
-            repaint();
-        }
-
-        public void componentShown(ComponentEvent e) {
-            repaint();
-        }
-
-
-        public void componentHidden(ComponentEvent e) {
-            repaint();
-        }
-    };
-
-}// </editor-fold>
-
+}
---
+++ src/vernamonsters/ResourceMap.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vernamonsters;
+
+import java.awt.Image;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Sweord
+ */
+public class ResourceMap {
+
+    HashMap<String, Image> imageResources;
+
+    public ResourceMap() {
+        imageResources = new HashMap<String, Image>();
+
+    }
+
+    /**
+     *
+     * @param resourceID : Location of the image in the package.
+     * @param Name : Name to refer to this image in the future.
+     */
+    public void addImage(String resourceID, String Name) {
+        Image temp;
+        try {
+            temp = ImageIO.read(this.getClass().getResource(resourceID));
+            imageResources.put(Name, temp);
+        } catch (IOException ex) {
+            Logger.getLogger(BattleScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Image getImage(String Name){
+        if(imageResources.containsKey(Name))return imageResources.get(Name);
+        else return null;
+        
+    }
+}
